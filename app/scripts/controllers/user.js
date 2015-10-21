9c6acb560158dd38e59526b0d3a5cd52f10cbb65
sebastian correa morales <correa.m.sebastian@gmail.com>
2015-10-21 07:50:53 -0500

    probado localmente

---
+++ app/scripts/controllers/user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularjsUnitTestApp.controller:UserCtrl
+ * @description
+ * # UserCtrl
+ * Controller of the angularjsUnitTestApp
+ */
+angular.module('angularjsUnitTestApp')
+  .controller('UserCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
